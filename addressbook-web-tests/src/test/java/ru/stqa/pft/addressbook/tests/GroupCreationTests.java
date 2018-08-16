a326cac192cf4e82c1c77458506ff2bc94035537
tmarmuzevich <42098155+tmarmuzevich@users.noreply.github.com>
2018-08-16 10:46:36 -0500

    создан класс ApplicationManager, куда перенесены основные функции

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -0,0 +1,18 @@
+package ru.stqa.pft.addressbook.tests;
+
+import org.testng.annotations.Test;
+import ru.stqa.pft.addressbook.model.GroupData;
+
+public class GroupCreationTests extends TestBase {
+
+  @Test
+  public void testGroupCreation() {
+
+    app.gotoGroupPage();
+    app.initGroupCreation();
+    app.fillGroupForm(new GroupData("Test1", "Test2", "Test3"));
+    app.submitGroupCreation();
+    app.returntoGroupPage();
+  }
+
+}
