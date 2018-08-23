1fc0d25042fd6fcdf64f7d5ca6d0770b232911d4
tmarmuzevich <42098155+tmarmuzevich@users.noreply.github.com>
2018-08-23 15:32:08 -0500

    Оптимизация заполнения форм (дефолтные значения в полях)

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/HelperBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/HelperBase.java
@@ -3,7 +3,6 @@ package ru.stqa.pft.addressbook.appmanager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoAlertPresentException;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class HelperBase {
   protected WebDriver wd;
@@ -18,8 +17,10 @@ public class HelperBase {
 
   protected void type(By locator, String text) {
     click(locator);
+    if (text != null){
     wd.findElement(locator).clear();
     wd.findElement(locator).sendKeys(text);
+    }
   }
 
   public boolean isAlertPresent() {
