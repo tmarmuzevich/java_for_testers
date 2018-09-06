3c5813b11a3e49d71407d31314b4d7121aba795c
tmarmuzevich <42098155+tmarmuzevich@users.noreply.github.com>
2018-09-06 14:07:48 -0500

    Замена списков множествами в тестах для групп

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -5,35 +5,31 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 
-import java.util.Comparator;
-import java.util.List;
+import java.util.Set;
 
 public class GroupModificationTests extends TestBase {
 
   @BeforeMethod
   public void ensurePreconditions() {
     app.goTo().groupPage();
-    if (app.group().list().size() == 0) {
+    if (app.group().all().size() == 0) {
       app.group().create(new GroupData().withName("Test1"));
     }
   }
 
-    @Test
-    public void testGroupModification () {
-      List<GroupData> before = app.group().list();
-      int index = before.size() - 1;
-      GroupData group = new GroupData()
-              .withId(before.get(index).getId()).withName("Test1").withHeader("Test2").withFooter("Test3");
-      app.group().modify(index, group);
-      List<GroupData> after = app.group().list();
-      Assert.assertEquals(after.size(), before.size());
+  @Test
+  public void testGroupModification() {
+    Set<GroupData> before = app.group().all();
+    GroupData modifiedGroup = before.iterator().next();
+    GroupData group = new GroupData()
+            .withId(modifiedGroup.getId()).withName("Test1").withHeader("Test2").withFooter("Test3");
+    app.group().modify(group);
+    Set<GroupData> after = app.group().all();
+    Assert.assertEquals(after.size(), before.size());
 
-      before.remove(index);
-      before.add(group);
-      Comparator<? super GroupData> byId = (g1, g2) -> Integer.compare(g1.getId(), g2.getId());
-      before.sort(byId);
-      after.sort(byId);
-      Assert.assertEquals(before, after);
+    before.remove(modifiedGroup);
+    before.add(group);
+    Assert.assertEquals(before, after);
 
-    }
   }
+}
