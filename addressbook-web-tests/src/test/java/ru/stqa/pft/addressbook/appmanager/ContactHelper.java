195045dfab619d076db9073553076b2f10cdfe68
tmarmuzevich <42098155+tmarmuzevich@users.noreply.github.com>
2018-09-06 11:34:24 -0500

    Внесены исправления в запрос поиска ФИО контакта для списка контактов

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
@@ -91,8 +91,8 @@ public class ContactHelper extends HelperBase {
     List<WebElement> elements = wd.findElements(By.name("entry"));
     for (WebElement element : elements) {
       int id = Integer.parseInt(element.findElement(By.tagName("input")).getAttribute("id"));
-      String lastName = element.findElement(By.xpath("//td[2]")).getText();
-      String firstName = element.findElement(By.xpath("//td[3]")).getText();
+      String lastName = element.findElement(By.xpath(".//td[2]")).getText();
+      String firstName = element.findElement(By.xpath(".//td[3]")).getText();
       ContactData contact = new ContactData(id, firstName, lastName, null, null, null, null, null, null, null);
       contacts.add(contact);
     }
