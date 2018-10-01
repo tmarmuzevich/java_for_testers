24a156d9fbd2d439984746e890dbd0f05cb6d0ab
tmarmuzevich <42098155+tmarmuzevich@users.noreply.github.com>
2018-10-01 12:13:40 -0500

    Добавлена возможность чтения входных данных из файла для теста создания группы

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/GroupDataGenerator.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/GroupDataGenerator.java
@@ -40,7 +40,7 @@ public class GroupDataGenerator {
   private void save(List<GroupData> groups, File file) throws IOException {
     Writer writer = new FileWriter(file);
     for (GroupData group : groups) {
-      writer.write(String.format("%s,%s,%s\n", group.getName(), group.getHeader(), group.getFooter()));
+      writer.write(String.format("%s;%s;%s\n", group.getName(), group.getHeader(), group.getFooter()));
     }
     writer.close();
   }
